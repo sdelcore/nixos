1297ed3afc55577cc988bd22892eb0d99962fc37
sdelcore <sdelcore@gmail.com>
2026-01-25 08:17:43 -0500

    add alt g to unlock zellij

--- home/modules/zellij.nix
+++ home/modules/zellij.nix
@@ -7,14 +7,17 @@
     };
   };
 
-  xdg.configFile = {
-    "zellij/config.kdl".text = # kdl
-      ''
-        default_mode "locked"
-        show_startup_tips false
-        keybinds {
-          unbind "Ctrl q"
-        }
-      '';
-  };
+  xdg.configFile."zellij/config.kdl".text = ''
+    default_mode "locked"
+    show_startup_tips false
+    keybinds {
+      unbind "Ctrl q"
+      locked {
+        bind "Alt g" { SwitchToMode "normal"; }
+      }
+      shared_except "locked" {
+        bind "Alt g" { SwitchToMode "locked"; }
+      }
+    }
+  '';
 }
